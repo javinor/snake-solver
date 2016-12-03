a26927eaa377e24027ac171b7f66d8828c414681
javinor <javinor@gmail.com>
2016-12-03 20:34:30 +0200

    vector functions to accept vectors

--- snakeCubeSolver.js
+++ snakeCubeSolver.js
@@ -3,11 +3,12 @@
 const units = require('./src/unit-vectors')
 const directions = require('./src/unit-names')
 
-const add = (x1, y1, z1, x2, y2, z2) => [x1 + x2, y1 + y2, z1 + z2]
-const dot = (x1, y1, z1, x2, y2, z2) => x1 * x2 + y1 * y2 + z1 * z2
+// vector operations
+const add = (u, v) => [u[0] + v[0], u[1] + v[1], u[2] + v[2]]
+const dot = (u, v) => u.reduce((sum, _, i) => sum + u[i] * v[i], 0)
 
-const perps = units.reduce((acc, unit) => {
-  acc[unit] = units.filter(u => dot(...u, ...unit) === 0)
+const unitToPerp = units.reduce((acc, unit) => {
+  acc[unit] = units.filter(u => dot(u, unit) === 0)
   return acc
 }, {})
 
@@ -15,27 +16,27 @@ const getInstructionsFromPath = (path) => {
   return path
     .map(vec => directions[vec])
     .filter((v, i, arr) => {
-      return i === 0 || v !== arr[i-1]
+      return i === 0 || v !== arr[i - 1]
     })
 }
 
 class Cube {
-  constructor(size) {
-    this.cube = Array.from(Array(size)).map(x => Array.from(Array(size)).map(y => [0,0,0]))
+  constructor (size) {
+    this.cube = Array.from(Array(size)).map(x => Array.from(Array(size)).map(y => [0, 0, 0]))
   }
-  has(x, y, z) { return this.cube[x][y][z] === 1 }
-  set(x, y, z) { this.cube[x][y][z] = 1 }
-  unset(x, y, z) { this.cube[x][y][z] = 0 }
-  isOutOfBounds(x, y, z) { 
+  has (x, y, z) { return this.cube[x][y][z] === 1 }
+  set (x, y, z) { this.cube[x][y][z] = 1 }
+  unset (x, y, z) { this.cube[x][y][z] = 0 }
+  isOutOfBounds (x, y, z) {
     const size = this.cube.length
-    return x < 0 || y < 0 || z < 0 || size <= x || size <= y || size <= z 
+    return x < 0 || y < 0 || z < 0 || size <= x || size <= y || size <= z
   }
 }
 
 // ///////////////////////////////////////////
 
 const getNextDirections = (direction, toTurn) => {
-  return toTurn ? perps[direction] : [direction]
+  return toTurn ? unitToPerp[direction] : [direction]
 }
 
 const recursiveSnakeSolver = (cube, snake, snakeIndex, location, direction) => {
@@ -46,10 +47,10 @@ const recursiveSnakeSolver = (cube, snake, snakeIndex, location, direction) => {
   const newDirections = getNextDirections(direction, snake[snakeIndex])
 
   for (let i = 0, len = newDirections.length; i < len; i++) {
-    const newLocation = add(...location, ...newDirections[i])
+    const newLocation = add(location, newDirections[i])
     if (!cube.isOutOfBounds(...newLocation) && !cube.has(...newLocation)) {
       let res = recursiveSnakeSolver(cube, snake, snakeIndex + 1, newLocation, newDirections[i])
-      if (res) return [newDirections[i]].concat(res)      
+      if (res) return [newDirections[i]].concat(res)
     }
   }
 
@@ -57,8 +58,8 @@ const recursiveSnakeSolver = (cube, snake, snakeIndex, location, direction) => {
 }
 
 const startingPointsBySize = {
-  3: [[0,0,0], [1,1,0], [1,1,1]],
-  4: [[0,0,0], [1,0,0], [1,1,0], [1,1,1]]
+  3: [[0, 0, 0], [1, 1, 0], [1, 1, 1]],
+  4: [[0, 0, 0], [1, 0, 0], [1, 1, 0], [1, 1, 1]]
 }
 
 const snakeSolver = (snake) => {
@@ -79,7 +80,7 @@ const snake = [0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0,
 // const snake = [0,0,1,1,0,1,1,1,0,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,0,0,1,1,1,1,0,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0]
 
 const startTime = Date.now()
-const res = snakeSolver(snake, 0, [0, 0, 0],  units[0])
+const res = snakeSolver(snake, 0, [0, 0, 0], units[0])
 
 if (res) {
   console.log(`Solution took ${Date.now() - startTime} ms`)
@@ -88,5 +89,4 @@ if (res) {
 } else {
   console.log('no solution found')
 }
-
 
